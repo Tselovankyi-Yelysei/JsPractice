c154c54129f246eb7c6374e64532df23d8552441
Tselovanskyi Yelisei <yeliseitselovanskyi@gmail.com>
2025-03-21 21:39:18 +0200

    practice14 optimised

--- src/js/practice14.js
+++ src/js/practice14.js
@@ -1,32 +1,58 @@
 const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
 function sortStudentsByGroups(arr) {
-    arr = arr.sort();
-    let teamsArr = [];
-    for (let i = 0; i < 3; i++) {
-        let threeStudents = [];
-        for (let i = teamsArr.length * 3; i < (teamsArr.length + 1) * 3; i++) {
-            threeStudents.push(arr[i]);
+    arr.sort();
+    let a = [], b = [], c = [], rest = [];
+    console.log(rest.length);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(students[i]);
         }
-        teamsArr.push(threeStudents);
-    }
-    console.log(teamsArr);
-    if (arr.length == 9) {
-        teamsArr.push(`Оставшиеся студенты: -`);
-    }
-    else if (arr.length < 9) {
-        console.log('Вы ввели недостаточное число игроков! Введите хотя бы 9!')
-    }
-    else {
-        let remainsStudents = 'Оставшиеся студенты:'
-        for (let i = 9; i < arr.length; i++) {
-            remainsStudents += ` ${arr[i]},`
+        else if (i < 6) {
+            b.push(students[i]);
         }
-        remainsStudents = remainsStudents.slice(0, -1);
-        teamsArr.push(remainsStudents);
+        else if (i < 9) {
+            c.push(students[i]);
+        }
+        else {
+            rest.push(students[i]);
+        }
     }
-    console.log(teamsArr);
-    return teamsArr;
+    console.log(rest.length);
+    return [a, b, c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
 }
 
-sortStudentsByGroups(students);
+console.log(sortStudentsByGroups(students));
+
+
+// function sortStudentsByGroups(arr) {
+//     arr = arr.sort();
+//     let teamsArr = [];
+//     for (let i = 0; i < 3; i++) {
+//         let threeStudents = [];
+//         for (let i = teamsArr.length * 3; i < (teamsArr.length + 1) * 3; i++) {
+//             threeStudents.push(arr[i]);
+//         }
+//         teamsArr.push(threeStudents);
+//     }
+//     console.log(teamsArr);
+//     if (arr.length == 9) {
+//         teamsArr.push(`Оставшиеся студенты: -`);
+//     }
+//     else if (arr.length < 9) {
+//         console.log('Вы ввели недостаточное число игроков! Введите хотя бы 9!')
+//     }
+//     else {
+//         let remainsStudents = 'Оставшиеся студенты:'
+//         for (let i = 9; i < arr.length; i++) {
+//             remainsStudents += ` ${arr[i]},`
+//         }
+//         remainsStudents = remainsStudents.slice(0, -1);
+//         teamsArr.push(remainsStudents);
+//     }
+//     console.log(teamsArr);
+//     return teamsArr;
+// }
+
+// sortStudentsByGroups(students);
